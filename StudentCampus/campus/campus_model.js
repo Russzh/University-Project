ad2a6f072075931a2f370847a199c21d30e66164
Russzh <47798938+Russzh@users.noreply.github.com>
2019-05-12 15:28:31 +0300

    Add files via upload

---
+++ StudentCampus/campus/campus_model.js
@@ -0,0 +1,13 @@
+﻿const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CampusSchema = new Schema({
+    _id: Schema.Types.ObjectId,
+    number: { type: Number, required: true },
+    adress: { type: String, required: true },
+    rooms: [{ type: Schema.Types.ObjectId, ref: 'Room' }]
+});
+
+
+// Export the model
+module.exports = mongoose.model('Campus', CampusSchema);
---
+++ StudentCampus/room/room_model.js
@@ -0,0 +1,13 @@
+﻿const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const CampusSchema = require('../campus/campus_model');
+
+const RoomSchema = new Schema({
+    number: { type: Number, required: true },
+    place_count: { type: Number, required: true },
+    _campus_id: { type: Schema.Types.ObjectId, ref: 'Campus' }
+});
+
+
+// Export the model
+module.exports = mongoose.model('Room', RoomSchema);
